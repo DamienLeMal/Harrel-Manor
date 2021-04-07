09bdb8b2a4c208feae881308842c00752117c724
Maxime Rousseau <36676003+Dexeat@users.noreply.github.com>
2021-04-07 09:48:03 +0200

    v1
    Déplacement opé !

--- Projet Chthulhu/Assets/Scripts/Exploration/deplacement.cs
+++ Projet Chthulhu/Assets/Scripts/Exploration/deplacement.cs
@@ -1,28 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class deplacement : MonoBehaviour
 {
+    NavMeshAgent agent;
 
-    private bool asArrived = true;
-    public GameObject targetPrefab;
-    private Vector3 targetPos;
+    public float rotateSpeedMovement = 0.1f;
+    float rotateVelocity;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        agent = gameObject.GetComponent<NavMeshAgent>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(1)&& asArrived == true) 
+        if(Input.GetMouseButtonDown(1))
         {
-            //asArrived = false;
-            targetPos = new Vector3(Input.mousePosition.x,0,Input.mousePosition.y);
-            Instantiate(targetPrefab, targetPos, transform.rotation);
+            RaycastHit hit;
+
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, Mathf.Infinity))
+            {
+                agent.SetDestination(hit.point);
+
+                Quaternion rotationToLookAt = Quaternion.LookRotation(hit.point - transform.position);
+                float rotationY = Mathf.SmoothDampAngle(transform.eulerAngles.y,rotationToLookAt.eulerAngles.y,ref rotateVelocity,rotateSpeedMovement * (Time.deltaTime * 5));
+                transform.eulerAngles = new Vector3(0, rotationY, 0);
+            }
         }
     }
 
