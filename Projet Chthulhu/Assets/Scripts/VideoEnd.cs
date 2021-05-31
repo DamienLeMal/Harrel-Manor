22c2685c743fe456a8729a2deaf202a20db40d18
Damien Le Mâl <36676003+Dexeat@users.noreply.github.com>
2021-05-31 16:19:10 +0200

    ajoute de l'intro
    hardcoder
    
    -intro trop lourde

---
+++ Projet Chthulhu/Assets/Scripts/VideoEnd.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VideoEnd : MonoBehaviour
+{
+    [SerializeField] GameObject targetVideo;
+    [SerializeField] private float timemax;
+    [SerializeField] private float timea;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (timea >= timemax)
+        {
+            targetVideo.SetActive(false);
+        }
+        else
+        {
+            timea += Time.deltaTime;
+        }
+    }
+    
+}
