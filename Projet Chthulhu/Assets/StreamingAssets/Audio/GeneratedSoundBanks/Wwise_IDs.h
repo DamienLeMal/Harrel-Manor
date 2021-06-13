3368603ce54732396480ea1bda0466f3cf9455bf
Léo Lemberger <moonduderecords@gmail.com>
2021-06-13 17:06:27 +0200

    Implementation de Wwise, Creation des mecaniques musicales dans Wwise... Et plus encore...

---
+++ Projet Chthulhu/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,191 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID PLAY_AMB_CLOCKS_LP = 1082785423U;
+        static const AkUniqueID PLAY_AMB_ROOMTONE_LP = 657997079U;
+        static const AkUniqueID PLAY_CHARA_VERO_FOOTSTEPS = 2026159004U;
+        static const AkUniqueID PLAY_COUP_CROSSE = 4198407779U;
+        static const AkUniqueID PLAY_DIAL_AILE_EST_BIBLIO_VERO = 2565330666U;
+        static const AkUniqueID PLAY_DIAL_AILE_EST_SALONS_VERO = 2192183219U;
+        static const AkUniqueID PLAY_DIAL_AILE_EST_TROUVE_NOTE = 326070264U;
+        static const AkUniqueID PLAY_DIAL_AILE_OUEST_CUISINE_VERO = 3265918847U;
+        static const AkUniqueID PLAY_DIAL_AILE_OUEST_DAGUE_MECANISME_VERO = 206013450U;
+        static const AkUniqueID PLAY_DIAL_AILE_OUEST_DAGUE_VERO = 2230653163U;
+        static const AkUniqueID PLAY_DIAL_AILE_OUEST_ENTRE_VERO = 2435664213U;
+        static const AkUniqueID PLAY_DIAL_AILE_OUEST_SALLE_A_MANGER_VERO = 3085051411U;
+        static const AkUniqueID PLAY_DIAL_ETAGE_TROUVE_NOTE_3_VERO = 3107429427U;
+        static const AkUniqueID PLAY_DIAL_HALL_DEAMBULE_VERO = 2189397941U;
+        static const AkUniqueID PLAY_DIAL_HALL_ENTEND_VOIX_1ERE_HALLU_VERO = 29555598U;
+        static const AkUniqueID PLAY_DIAL_HALL_ENTEND_VOIX_2EME_HALLU_VERO = 2241837674U;
+        static const AkUniqueID PLAY_DIAL_HALL_ENTEND_VOIX_ALLIE_VERO = 4218302083U;
+        static const AkUniqueID PLAY_DIAL_HALL_ENTRE_VERO = 3649415520U;
+        static const AkUniqueID PLAY_DIAL_MANOIR_CROISE_PR_TRES_VERO = 1521177110U;
+        static const AkUniqueID PLAY_DIAL_MANOIR_DECOUVRE_CADAVRE_VERO = 2182407155U;
+        static const AkUniqueID PLAY_DIAL_MANOIR_DECOUVRE_PORTE_FERMEE_VERO = 1678280368U;
+        static const AkUniqueID PLAY_DIAL_MANOIR_PORTE_FERMEE_VERO = 1786711616U;
+        static const AkUniqueID PLAY_DIAL_MANOIR_TROUVE_CLE_VERO = 1575838255U;
+        static const AkUniqueID PLAY_ENEMY_PRETRE_FOOTSTEPS = 4108922607U;
+        static const AkUniqueID PLAY_ENFIELD = 2591643805U;
+        static const AkUniqueID PLAY_GAME_OVER_MUSIC = 1790908285U;
+        static const AkUniqueID PLAY_GPE_DOOR_CLOSE = 408225350U;
+        static const AkUniqueID PLAY_GPE_DOOR_OPEN = 4164744722U;
+        static const AkUniqueID PLAY_GPE_GRAB_KEY = 1064879809U;
+        static const AkUniqueID PLAY_GPE_GRAB_PAPER = 1966156210U;
+        static const AkUniqueID PLAY_GPE_GRAB_WEAPON_GUN = 64609095U;
+        static const AkUniqueID PLAY_GPE_GRAB_WEAPON_MELEE = 3512333401U;
+        static const AkUniqueID PLAY_GPE_STINGER_STRANGE_DOOR_OPENS_DISTANT = 2570176209U;
+        static const AkUniqueID PLAY_KNIFE = 2169194149U;
+        static const AkUniqueID PLAY_MUSIC_MAIN_MENU = 3968446557U;
+        static const AkUniqueID PLAY_PRETRE_DEATH = 3721076595U;
+        static const AkUniqueID PLAY_PUMPGUN = 1111312230U;
+        static const AkUniqueID PLAY_PUMPGUN_LOAD = 209240245U;
+        static const AkUniqueID PLAY_SFX_CAMERA_SWIPE_LEFT = 4282659160U;
+        static const AkUniqueID PLAY_SFX_CAMERA_SWIPE_RIGHT = 2046201231U;
+        static const AkUniqueID PLAY_SPELL_BLACK = 3646898964U;
+        static const AkUniqueID PLAY_SPELL_FIRE = 3251764843U;
+        static const AkUniqueID PLAY_SW_MUSIC_CTHULHU = 2800641524U;
+        static const AkUniqueID PLAY_SYRINGE = 3665765901U;
+        static const AkUniqueID PLAY_VICTORY_MUSIC = 2141970788U;
+        static const AkUniqueID PLAY_VO_PRETRE_ATTACK = 240115491U;
+        static const AkUniqueID PLAY_VO_PRETRE_ERRE_LP = 3084755036U;
+        static const AkUniqueID PLAY_VO_PRETRE_HURT = 3704012044U;
+        static const AkUniqueID PLAY_VO_VERO_DEATH = 1619418399U;
+        static const AkUniqueID PLAY_VO_VERO_HURT = 3924178152U;
+        static const AkUniqueID PLAY_WEBLEY = 2757431146U;
+        static const AkUniqueID PLAY_WEBLEY_RELOAD = 3573264990U;
+        static const AkUniqueID SET_MUSIC_STATE_COMBAT = 896606900U;
+        static const AkUniqueID SET_MUSIC_STATE_PAUSE_OFF = 611179312U;
+        static const AkUniqueID SET_MUSIC_STATE_PAUSE_ON = 3006178586U;
+        static const AkUniqueID SET_MUSIC_STATE_STEALTH_OFF = 3320324331U;
+        static const AkUniqueID SET_MUSIC_STATE_STEALTH_ON = 2199726423U;
+        static const AkUniqueID STOP_AMB_CLOCKS = 2556918918U;
+        static const AkUniqueID STOP_AMB_ROOMTONE_LP = 730634393U;
+        static const AkUniqueID STOP_MUSIC_MAIN_MENU = 2913477591U;
+        static const AkUniqueID STOP_VO_PRETRE_ERRE_LP = 2599990218U;
+    } // namespace EVENTS
+
+    namespace STATES
+    {
+        namespace ST_GAME_STATE
+        {
+            static const AkUniqueID GROUP = 42620231U;
+
+            namespace STATE
+            {
+                static const AkUniqueID COMBAT = 2764240573U;
+                static const AkUniqueID EXPLORATION = 2582085496U;
+                static const AkUniqueID NONE = 748895195U;
+            } // namespace STATE
+        } // namespace ST_GAME_STATE
+
+        namespace ST_IN_GAME_PAUSE
+        {
+            static const AkUniqueID GROUP = 3819847180U;
+
+            namespace STATE
+            {
+                static const AkUniqueID IN_GAME = 2967546505U;
+                static const AkUniqueID NONE = 748895195U;
+                static const AkUniqueID PAUSE = 3092587493U;
+            } // namespace STATE
+        } // namespace ST_IN_GAME_PAUSE
+
+        namespace ST_STEALTH
+        {
+            static const AkUniqueID GROUP = 1610983308U;
+
+            namespace STATE
+            {
+                static const AkUniqueID NONE = 748895195U;
+                static const AkUniqueID NORMAL = 1160234136U;
+                static const AkUniqueID STEALTH = 2909291642U;
+            } // namespace STATE
+        } // namespace ST_STEALTH
+
+    } // namespace STATES
+
+    namespace SWITCHES
+    {
+        namespace SW_FOOTSTEPS_PRETRES
+        {
+            static const AkUniqueID GROUP = 85646123U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID CARPET = 2412606308U;
+                static const AkUniqueID TILE = 2637588553U;
+                static const AkUniqueID WOOD = 2058049674U;
+                static const AkUniqueID WOOD_CREAKY = 3917875680U;
+            } // namespace SWITCH
+        } // namespace SW_FOOTSTEPS_PRETRES
+
+        namespace SW_FOOTSTEPS_VERO
+        {
+            static const AkUniqueID GROUP = 182727268U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID CARPET = 2412606308U;
+                static const AkUniqueID TILE = 2637588553U;
+                static const AkUniqueID WOOD = 2058049674U;
+                static const AkUniqueID WOOD_CREAKY = 3917875680U;
+            } // namespace SWITCH
+        } // namespace SW_FOOTSTEPS_VERO
+
+    } // namespace SWITCHES
+
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID RTPC_DISTANCE = 262290038U;
+        static const AkUniqueID RTPC_HEALTH = 2474978741U;
+        static const AkUniqueID RTPC_OCCLUSION = 1664992182U;
+        static const AkUniqueID RTPC_SIDECHAIN_SFX_TO_AMBIANCES = 3452052565U;
+    } // namespace GAME_PARAMETERS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID EAST_WING_SNK = 1351887207U;
+        static const AkUniqueID GENERAL_SNK = 456350576U;
+        static const AkUniqueID HALL_SNK = 3935067443U;
+        static const AkUniqueID MAIN_MENU_SNK = 2634361011U;
+        static const AkUniqueID MUSIC_SNK = 2767260189U;
+        static const AkUniqueID WEST_WING_SNK = 353002933U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID AMBIANCE = 2981377429U;
+        static const AkUniqueID CHARA = 434268486U;
+        static const AkUniqueID ENEMIES = 2242381963U;
+        static const AkUniqueID GPE = 831326253U;
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+        static const AkUniqueID MUSIC = 3991942870U;
+        static const AkUniqueID REVERBS = 3545700988U;
+        static const AkUniqueID SFX = 393239870U;
+        static const AkUniqueID UI = 1551306167U;
+        static const AkUniqueID VO = 1534528548U;
+        static const AkUniqueID VO_3D = 1701182122U;
+    } // namespace BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
